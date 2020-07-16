8975037e294c624618a797e433d6b85fb68b0aee
Jin Kim <jinwoov@gmail.com>
2020-07-16 07:15:41 -0700

    SB2

--- Basics/BST/BST/Program.cs
+++ Basics/BST/BST/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BST.Class;
+using System;
 
 namespace BST
 {
@@ -6,7 +7,22 @@ namespace BST
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Insert(bst.Root, 5);
+            bst.Insert(bst.Root, 4);
+            bst.Insert(bst.Root, 3);
+            bst.Insert(bst.Root, 7);
+            bst.Insert(bst.Root, 10);
+
+
+            BT.PreOrder(bst.Root);
+            Console.WriteLine();
+            BT.InOrder(bst.Root);
+            Console.WriteLine();
+            BT.PostOrder(bst.Root);
+            Console.WriteLine();
+            BT.BreathFirst(bst.Root);
+
         }
     }
 }
